8c9cfdaf3cf86e8a6ee98a1cf825a3aef877d2cf
chushi0 <wangyongchao49@sina.com>
2020-12-08 14:16:22 +0800

    基本输入功能

--- app/src/main/java/com/pmbp/pmbpime/IMEService.java
+++ app/src/main/java/com/pmbp/pmbpime/IMEService.java
@@ -3,6 +3,8 @@ package com.pmbp.pmbpime;
 import android.annotation.SuppressLint;
 import android.inputmethodservice.InputMethodService;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class IMEService extends InputMethodService {
     @SuppressLint({"ClickableViewAccessibility", "InflateParams"})
@@ -16,13 +18,30 @@ public class IMEService extends InputMethodService {
         setInputView(inputView);
         setCandidatesView(candidatesView);
 
-        inputView.setOnTouchListener((v, event) -> {
-            setCandidatesViewShown(true);
-            return false;
-        });
-        candidatesView.setOnTouchListener((v, event) -> {
-            setCandidatesViewShown(false);
-            return false;
-        });
+        setupListener(inputView, R.id.qwe);
+        setupListener(inputView, R.id.rtyu);
+        setupListener(inputView, R.id.iop);
+        setupListener(inputView, R.id.asd);
+        setupListener(inputView, R.id.fgh);
+        setupListener(inputView, R.id.jkl);
+        setupListener(inputView, R.id.zxc);
+        setupListener(inputView, R.id.vbn);
+        setupListener(inputView, R.id.m);
+    }
+
+    private void setupListener(View inputView, int resId) {
+        ViewGroup viewGroup = inputView.findViewById(resId);
+        int count = viewGroup.getChildCount();
+        for (int i = 0; i < count; i++) {
+            View view = viewGroup.getChildAt(i);
+            view.setOnClickListener(this::keyboardClick);
+        }
+    }
+
+    private void keyboardClick(View view) {
+        if (view instanceof TextView) {
+            String text = ((TextView) view).getText().toString();
+            getCurrentInputConnection().commitText(text, text.length());
+        }
     }
 }
